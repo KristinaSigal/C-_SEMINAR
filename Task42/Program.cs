027198e30387713aaa53bdf0954cfdc0642447e5
kristina.sigal <kulrikh@gmail.com>
2022-08-26 22:43:07 +0300

    Add Tasks 41, 42, 43 &45

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -4,23 +4,33 @@
 // 3 -> 11
 // 2 -> 10
 
-Console.WriteLine("Введите целое число");
-int num = Convert.ToInt32(Console.ReadLine());
+int num = 13;
 
-int ChangeToBin(int a)
+void DecToBin(int n)
 {
-    // int b = a%2;
-    // int c = b/2;   
-    int result = 0;
-    int mult = 1;
-    while (a > 0)
-    {
-        result += a % 2 * mult;   //тоже что и result = result + a%2
-        a /= 2;
-        mult *= 10; // тоже что и mult = mult * 10
-    }
-    return result;
+    if (n == 0) return;
+    DecToBin(n / 2);
+    Console.Write(n % 2);
 }
+DecToBin(num);
 
-int res = ChangeToBin(num);
-System.Console.WriteLine($"{res}");
+// Console.WriteLine("Введите целое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int ChangeToBin(int a)
+// {
+//     // int b = a%2;
+//     // int c = b/2;   
+//     int result = 0;
+//     int mult = 1;
+//     while (a > 0)
+//     {
+//         result += a % 2 * mult;   //тоже что и result = result + a%2
+//         a /= 2;
+//         mult *= 10; // тоже что и mult = mult * 10
+//     }
+//     return result;
+// }
+
+// int res = ChangeToBin(num);
+// System.Console.WriteLine($"{res}");
