719f5349b9a2dd5f7cec792ed2496943eef19527
kristina.sigal <kulrikh@gmail.com>
2022-09-06 16:56:16 +0300

    Edit Task 67

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -5,13 +5,22 @@
 
 Console.WriteLine("Введите целое положительное число");
 int number = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-int SumDigit(int num)
+
+// int SumDigit(int num)
+// {
+//     int sum = 0;
+//     if (num/10 <= 0) return sum +=num % 10;
+//     sum += SumDigit(num/10);
+//     return sum += num % 10;
+// }
+
+int SumDigit2(int num)
 {
-    if (num/10 <= 0) return sum +=num % 10;
-    sum += SumDigit(num/10);
-    return sum += num % 10;
+    int sum = num % 10;
+    if (num > 0) sum += SumDigit2(num / 10);
+    return sum;
 }
 
-int res = SumDigit(number);
+//int res = SumDigit(number);
+int res = SumDigit2(number);
 Console.WriteLine(res);
